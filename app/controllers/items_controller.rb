5d5324607607f38daae90231b2493c9a1e2fc2c9
thomas dubiel <github email address>
2020-05-01 19:31:12 -0500

    add show action and controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,10 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all 
+    @items = Item.all
   end
 
   def show
-
+    @item = Item.find(params[:id])
   end
 
   def new
