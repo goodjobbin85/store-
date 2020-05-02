cfe7c5f2004ad137fa7990de41299b8d38960a31
thomas dubiel <github email address>
2020-05-01 21:20:42 -0500

    finish in items controller actions and private methods

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,29 +1,53 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def index
     @items = Item.all
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def new
+    @item = Item.new
+  end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
   end
 
   def edit
 
   end
 
-  def create
 
-  end
 
   def update
-
+    if @item.update(set_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end 
   end
 
   def destroy
+    @item.destroy
+    redirect_to items_path
+  end
 
+  private
+
+  def set_params
+    params.require(:item).permit(:name, :description, :image, :quantity, :price, :sku)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
