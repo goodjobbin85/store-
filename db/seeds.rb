716427dd431c6cc9969e134e7a6970c3175c6ff1
thomas dubiel <github email address>
2020-05-01 19:21:52 -0500

    seed item data, install faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Item.delete_all
+
+50.times do
+  Item.create!(
+    name: Faker::Book.title,
+    description: "random description",
+    sku: rand(10 ** 10),
+    image: "default image",
+    quantity: rand(10 ** 4),
+    price: 9.99
+  )
+end
