8c0e4bde78bef7e6f005f43418f938c7d0950282
thomas dubiel <github email address>
2020-05-05 16:27:04 -0500

    create item show page links

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_205551) do
+ActiveRecord::Schema.define(version: 2020_05_05_205839) do
 
   create_table "items", force: :cascade do |t|
     t.string "name", null: false
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2020_05_01_205551) do
     t.string "image"
     t.integer "quantity", null: false
     t.decimal "price", precision: 10, scale: 2, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
