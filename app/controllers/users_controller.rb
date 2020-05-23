9dd9179f5cc34a1942415a6b5c0b610601486ffd
thomas dubiel <github email address>
2020-05-23 15:38:09 -0500

    edit action and links on show page to edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,5 +13,20 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-  end 
+  end
+
+  def update
+    @user = User.new(user_params)
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
