11dbd8e7d90c832678532aaecc3744446e972662
thomas dubiel <github email address>
2020-05-24 11:13:14 -0500

    static controller views routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'static#home'
+  
+  get 'static/home'
   resources :shops do
     resources :items
   end
-  
+
   resources :items
   resources :users
 
