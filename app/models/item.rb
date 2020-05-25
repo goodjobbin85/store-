4c9f0cb1f8c3a84c9d726cae92309e653a634b2e
thomas dubiel <github email address>
2020-05-25 11:00:34 -0500

    add category controller and model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
   belongs_to :shop
+  belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_23_220657) do
+ActiveRecord::Schema.define(version: 2020_05_25_154255) do
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name", null: false
