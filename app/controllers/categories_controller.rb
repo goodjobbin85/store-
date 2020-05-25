4749c9c20fdb86ac6270ca9914500e21ca1014de
thomas dubiel <github email address>
2020-05-25 15:24:22 -0500

    fill out categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,47 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = User.all
+  end
+
+  def show
+    @category = User.find(params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.create(category_params)
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to category_path(@category)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path 
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
