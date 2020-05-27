75644ba8091f6e9ad37cfac20f2e437ab5ca5f8c
thomas dubiel <github email address>
2020-05-27 06:22:39 -0500

    shop controller actions and views

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,10 +1,52 @@
 class ShopsController < ApplicationController
+  before_action :set_shop, only: [:show, :edit, :update, :destroy]
+
   def index
     @shops = Shop.all
   end
 
   def show
+
+  end
+
+  def new
+    @shop = Shop.new
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      redirect_to shop_path(@shop)
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @shop.update(shop_params)
+      redirect_to shop_path(@shop)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @shop.destroy
+    redirect_to shops_path
+  end
+
+  private
+
+  def set_shop
     @shop = Shop.find(params[:id])
   end
-  
+
+  def shop_params
+    params.require(:shop).permit(:name)
+  end
+
 end
