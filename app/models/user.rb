72e81d5a7e6c3d16b72ebcb3850e0f32dfb272f9
thomas dubiel <github email address>
2020-06-09 09:54:38 -0500

    create user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :password, presence: true, length: { minimum: 8 }
+  EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: EMAIL_REGEX, message: "Email must be properly formatted" }
+
   has_secure_password
 end
