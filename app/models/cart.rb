8137b071b600de8e7abe02dee896742e2c01e8f9
thomas dubiel <github email address>
2020-06-13 06:03:44 -0500

    add_item method in cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,14 @@
 class Cart < ApplicationRecord
   belongs_to :user
   has_many :line_items, dependent: :destroy
+
+  def add_item(item)
+    current_item = line_items.find_by(item_id: item.id)
+    if current_item
+      current_item += 1
+    else
+      current_item = line_items.build(item_id: item.id)
+    end
+    current_item
+  end
 end
