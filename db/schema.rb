fa3e11bca3902bc7358727d2033884f070e88404
thomas dubiel <github email address>
2020-06-12 23:14:59 -0500

    add quantity to line items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_12_110324) do
+ActiveRecord::Schema.define(version: 2020_06_13_040948) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_06_12_110324) do
     t.integer "cart_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "quantity", default: 1
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["item_id"], name: "index_line_items_on_item_id"
   end
